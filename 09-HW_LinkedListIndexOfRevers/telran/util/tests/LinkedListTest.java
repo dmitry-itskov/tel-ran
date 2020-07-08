71f8b1086971c6b33bf48840631304b3e3de2647
dmitry-itskov <61296029+dmitry-itskov@users.noreply.github.com>
2020-07-08 18:05:32 +0300

    Update LinkedListTest.java

--- 09-HW_LinkedListIndexOfRevers/telran/util/tests/LinkedListTest.java
+++ 09-HW_LinkedListIndexOfRevers/telran/util/tests/LinkedListTest.java
@@ -1,173 +1,161 @@
-package telran.util;
+package telran.util.tests;
 
-public class LinkedList<T> {
-	private static class Node<T> {
-		public T obj;
-		public Node<T> next;
-		public Node<T> prev;
+import static org.junit.jupiter.api.Assertions.*;
 
-		public Node(T obj) {
-			this.obj = obj;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import telran.util.LinkedList;
+
+class LinkedListTest {
+
+	int [] arrayInt = {10,-7, 20, 9, 13, 18};
+	String [] arrayStr = {"abd","lm", "hello"};
+	LinkedList<Integer> listInt;
+	LinkedList<String> listStr;
+	@BeforeEach
+	void setUp() {
+		listInt = new LinkedList<>();
+		listStr = new LinkedList<>();
+		for(int i = 0; i < arrayInt.length; i++) {
+			listInt.add(arrayInt[i]);
 		}
-	}
-
-	private Node<T> head; // reference to the first node
-	private Node<T> tail; // reference to the first node
-	private int size;
-	public void add(T obj) {
-		Node<T> node = new Node<>(obj);
-		if (head == null) {
-			head = tail = node;
-		} else {
-			node.prev = tail;
-			tail.next = node;
-			tail = node;
+		for(int i = 0; i < arrayStr.length; i++) {
+			listStr.add(arrayStr[i]);
 		}
-		size++;
-	}
-
-	public int size() {
-		return size;
-	}
-
-	public T get(int index) {
-		if (index < 0 || index >= size) {
-			return null;
-		}
-//		return (index < size / 2) ?  getFromLeft(index) : getFromRight(index);
-		Node<T> nodeIndex = getNode(index);
-		return nodeIndex.obj;
-	}
-
-	private Node<T> getNode(int index) {
-		return (index < size / 2) ? getFromLeft(index) : getFromRight(index);
-	}
-
-	private Node<T> getFromRight(int index) {
-		Node<T> current = tail;
-		for (int i = size - 1; i > index; i--) {
-			current = current.prev;
-		}
-		return current;
-	}
-
-	private Node<T> getFromLeft(int index) {
-		Node<T> current = head;
-		for (int i = 0; i < index; i++) {
-			current = current.next;
-		}
-		return current;
-	}
-
-	public boolean add(int index, T obj) {
-		if (index < 0 || index > size) {
-			return false;
-		}
-		if (index == 0) {
-			addHead(obj);
-			size++;
-		} else if(index == size){
-			add(obj);
-		} else {
-			addMiddle(index, obj);
-			size++;
-		}
-		// size++;
-		return true;
-	}
-
-	private void addMiddle(int index, T obj) {
-		Node<T> node = new Node<>(obj);
-		Node<T> nodeIndex = getNode(index);
-		Node<T> nodePrev = nodeIndex.prev;
-		nodePrev.next = node;
-		node.prev = nodePrev;
-		nodeIndex.prev = node;
-		node.next = nodeIndex;
-	}
-	
-	private void addHead(T obj) {
-		Node<T> node = new Node<>(obj);
-		head.prev = node;
-		node.next = head;
-		head = node;
-	}
-
-
-
-	public T remove(int index) {
-		if (index < 0 || index >= size) {
-			return null;
-		}
-		Node<T> node = getNode(index);
-		T res = node.obj;		
-		if (head == tail) {
-			head = tail = null;
-		} else {
-			if (index == 0) {
-				removeHead();
-			} else if (index == size - 1) {
-				removeTail();
-			} else {
-				removeNode(node);
-			}
-		}
-			
-			
-		size--;
-		return res;
-	}
-	
-	private void removeNode(Node<T> node) {
-		Node<T> nodeNext = node.next;
-		Node<T> nodePrev = node.prev;
-		nodePrev.next = nodeNext;
-		nodeNext.prev = nodePrev;
 		
 	}
-
-	private void removeTail() {
-		tail = tail.prev;
-		tail.next = null;
-	}
-	
-	private void removeHead() {
-		head = head.next;
-		head.prev = null;
-	}
-	
-	public int indexOf(Object pattern) {
-		Node <T> currentNode = head;
-		for (int i = 0; i < size; i++) {
-			if (currentNode.obj.equals(pattern)) {
-				return i;
+		@Test
+		void testAddGet() {
+			assertEquals(arrayInt.length, listInt.size());
+			assertEquals(arrayStr.length, listStr.size());
+			for (int i = 0; i < arrayInt.length; i++) {
+				assertEquals(arrayInt[i], listInt.get(i));
 			}
-			currentNode = currentNode.next;
-		}
-		return -1;
-	}
-	
-	public int lastIndexOf(Object pattern) {
-		Node <T> currentNode = tail;
-		for (int i = size-1; i >= 0; i--) {
-			if (currentNode.obj.equals(pattern)) {
-				return i;
+			for (int i = 0; i < arrayStr.length; i++) {
+				assertEquals(arrayStr[i], listStr.get(i));
 			}
-			currentNode = currentNode.prev;
+			assertEquals(null, listInt.get(50));
+			assertEquals(null, listInt.get(-50));
 		}
-		return -1;
-	}
-	
-	public void revers() {
-		Node <T> leftNode = head;
-		Node <T> rightNode = tail;
-		for (int i = 0; i < size/2; i++) {
-			T tempObj = leftNode.obj;
-			leftNode.obj = rightNode.obj;
-			rightNode.obj = tempObj;
-			leftNode = leftNode.next;
-			rightNode = rightNode.prev;
+		
+		
+		@Test
+		void testAddIndex() {
+			int [] expected = {10,-7, 20, 9, 44, 22, 13, 18}; 
+			assertTrue(listInt.add(4, 22));
+			assertTrue(listInt.add(4, 44));
+			for (int i = 0; i < expected.length; i++) {
+				assertEquals(expected[i], listInt.get(i));
+			}
+			assertFalse(listInt.add(-4, 22));
+			assertFalse(listInt.add(400,22));
 		}
-	}
+		
+		@Test
+		void testAddHead() {
+			int [] expected = {44, 22, 10,-7, 20, 9, 13, 18}; 
+			assertTrue(listInt.add(0, 22));
+			assertTrue(listInt.add(0, 44));
+			for (int i = 0; i < expected.length; i++) {
+				assertEquals(expected[i], listInt.get(i));
+			}
+		}
+		
+		
+		@Test
+		void testAddTail() {
+			int [] expected = {10,-7, 20, 9, 13, 18, 22, 44}; 
+			assertTrue(listInt.add(6, 22));
+			assertTrue(listInt.add(7, 44));
+			for (int i = 0; i < expected.length; i++) {
+				System.out.println(listInt.get(i));
+				assertEquals(expected[i], listInt.get(i));
+			}
+		}
+		
+		
+		@Test
+		void testRemoveIndex() {
+			int [] expected = {10,-7, 9, 13, 18}; 
+			assertEquals(20, listInt.remove(2));
+			for (int i = 0; i < expected.length; i++) {
+				assertEquals(expected[i], listInt.get(i));
+			}
+			assertNull(listInt.remove(-4));
+			assertNull(listInt.remove(400));
+		}
+		
+		@Test
+		void testRemoveIndexTail() {
+			int [] expected = {-7, 20, 9, 13}; 
+			assertEquals(18, listInt.remove(5));
+			assertEquals(10, listInt.remove(0));
+			for (int i = 0; i < expected.length; i++) {
+				assertEquals(expected[i], listInt.get(i));
+			}
+			assertEquals(-7, listInt.remove(0));
+			assertEquals(20, listInt.remove(0));
+			assertEquals(9, listInt.remove(0));
+			assertEquals(13, listInt.remove(0));
+			assertNull(listInt.remove(-4));
+			assertNull(listInt.remove(400));
+		}
+		
+		@Test
+		void testGetWrongIdex() {
+			assertEquals(null, listInt.get(-1));
+			assertEquals(null, listInt.get(500));
+		}
+		
+		@Test
+		void testIndexOf() {
+			int[] expected = {10,-7, 20, 9, 13, 18};
+			for (int i = 0; i < expected.length; i++) {
+				int pattern = expected[i];
+				assertEquals(i, listInt.indexOf(pattern));
+			}
+			assertEquals(-1, listInt.indexOf(-100));
+			assertEquals(-1, listInt.indexOf(100));
+		}
+		
+		@Test
+		void testLastIndexOf() {
+			
+			int[] expected = {10,-7, 20, 9, 13, 18};
+			
+			assertEquals (4, listInt.lastIndexOf(13));
+			assertEquals (5, listInt.lastIndexOf(18));
+			
+			for (int i = 0; i < expected.length; i++) {
+				int pattern = expected[i];
+				assertEquals(i, listInt.lastIndexOf(pattern));
+			}
+			
+			assertEquals(-1, listInt.lastIndexOf(1));
+			assertEquals(-1, listInt.lastIndexOf(2));
+			
+			listInt.add(5, 20);
+			assertEquals(5, listInt.lastIndexOf(20));
+			assertEquals(-1, listInt.lastIndexOf(-100));
+			assertEquals(-1, listInt.lastIndexOf(100));
+		}
+		
+		@Test
+		void testRevers() {
+			int[] expected = { 18, 13, 9, 20, -7, 10 };
+			listInt.revers();
+			for (int i = 0; i < expected.length; i++) {
+				assertEquals(expected[i], listInt.get(i));
+			}
+			listInt.add(5, 20);
+			listInt.revers();
+			int[] expected2 = { 10, 20, -7, 20, 9, 13, 18 };
+			for (int i = 0; i < expected.length; i++) {
+				assertEquals(expected2[i], listInt.get(i));
+			}
 
+		}
+		
 }
+
